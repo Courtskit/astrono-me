500d5bcfc08e1a4e026009b1c0ec69a94d2eb4a8
Courtskit <courtskit@gmail.com>
2022-01-19 13:20:20 -0800

    Make function to show images from a month back

--- api.js
+++ api.js
@@ -10,18 +10,26 @@ function photo(obj) {
 }
 
 async function handler(req, res) {
-  let url = `https://api.nasa.gov/planetary/apod?&start_date=2022-01-01`;
+  let startDate = new Date();
+  let day = String(startDate.getDate()).padStart(2, '0');
+  let month = String(startDate.getMonth() + 1).padStart(2, '0');
+  let year = startDate.getFullYear();
+
+  if(month == "01") {
+    month = "12";
+    year--;
+  }
+
+  startDate = year + '-' + month + '-' + day;
+
+  let url = `https://api.nasa.gov/planetary/apod?&start_date=${startDate}`;
   let data = await superagent
     .get(url)
     .query({ api_key: process.env.NASA_API_KEY })
     .catch();
 
-  console.log(data);
-
-  let photos = data.body.map(obj => new photo(obj));
+  let photos = data.body.reverse().map(obj => new photo(obj));
 
   res.render("index", { photos: photos })
 }
 module.exports = handler;
-
-// https://api.nasa.gov/planetary/apod?
